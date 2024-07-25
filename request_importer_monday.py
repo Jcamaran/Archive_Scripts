d59866ac8d8041de0656e9a35cb41df9b3c71780
Joaquin C <128087254+Jcamaran@users.noreply.github.com>
2024-07-24 21:25:24 -0400

    Update request_importer_monday.py

--- request_importer_monday.py
+++ request_importer_monday.py
@@ -35,49 +35,171 @@ def get_all_rows(worksheet):
 
 
 def enter_data_from_excel(rows):
-    for i, row in enumerate(rows[1:2], start=1):
-        request_from = row[5].value
+    for i, row in enumerate(rows[65:68], start=1):
         request_number = row[0].value
-        description = row[7].value
-        due_date = row[8].value
+        work_type = row[1].value
+        date_entered = row[2].value
+        clnd_tracking = row[3].value
+        request_source = row[4].value
+        request_from = row[5].value
+        subject = row[6].value
+        model = row[7].value
+        description = row[8].value
+        due_date = row[9].value
+        critical = row[10].value
+        contact_email = row[11].value
+        assigned_archivist = row[12].value
+        assigned_date = row[13].value
+        request_status = row[14].value
+        date_completed = row[15].value
+        tracking_info = row[16].value
+        material_link = row[17].value
+        presentation_date_1 = row[18].value
+        presentation_audience = row[19].value
+        presentation_date_2 = row[20].value
+
+        
 
         print(f"Row Number {i}: ")
         print(f"Request Number: {request_number}")
         print(f"Description: {description}")
 
-        if isinstance(due_date, datetime.datetime):
-            formatted_due_date = due_date.strftime('%Y-%m-%d')
-            print(f"Due Date: {formatted_due_date}")
 
-        # GraphQL mutation with variables
-        importing_query = """
-        mutation($board_id: ID!, $item_name: String!, $column_values: JSON!) {
-          create_item(board_id: $board_id, item_name: $item_name, columnVals: $column_values) {
+        if isinstance(due_date, datetime.datetime):
+            formatted_due_date = due_date.strftime('%m-%d-%Y')
+            print(f"Due Date: {formatted_due_date}")
+        else:
+            formatted_due_date = None
+        
+        if isinstance(date_entered, datetime.datetime):
+            formatted_date_entered = date_entered.strftime('%m-%d-%Y')
+            print(f"Due Date: {formatted_date_entered}")
+        else:
+            formatted_date_entered = None
+
+        if isinstance(assigned_date, datetime.datetime):
+            formatted_assigned_date = assigned_date.strftime('%m-%d-%Y')
+            print(f"Due Date: {formatted_assigned_date}")
+        else:
+            formatted_assigned_date = None
+        
+        if isinstance(date_completed, datetime.datetime):
+            formatted_date_completed = date_completed.strftime('%m-%d-%Y')
+            print(f"Due Date: {formatted_date_completed}")
+        else:
+            formatted_date_completed = None
+
+        if critical == "Y":
+            formatted_critical = "YES"
+        else:
+            formatted_critical = "NO"
+    
+
+        # Create the item
+        create_item_query = """
+        mutation($board_id: ID!, $item_name: String!) {
+          create_item(board_id: $board_id, item_name: $item_name) {
             id
           }
         }
         """
-        variables = {
-            "board_id": 6951881761,
-            "item_name": f"Request:{request_from}",
-            'columnVals' : json.dumps({
-                'status' : {'label' : 'Done'},
-                'date4' : {'date' : due_date}
-            })
+        create_item_variables = {
+            "board_id": "6951881761",  # Ensure the board ID is a string
+            "item_name": f"Request: {subject}"
         }
-        data = {'query': importing_query, 'variables': variables}
+        create_item_data = {'query': create_item_query, 'variables': create_item_variables}
 
-        # Make the request and handle the response
         try:
-            response = requests.post(url=apiUrl, json=data, headers=headers)
+            response = requests.post(url=apiUrl, json=create_item_data, headers=headers)
             response_data = response.json()
             if 'errors' in response_data:
                 print(f"Error creating item: {response_data['errors']}")
             else:
-                print(f"Item created with ID: {response_data['data']['create_item']['id']}")
+                item_id = response_data['data']['create_item']['id']
+                print(f"Item created with ID: {item_id}")
+
+                create_update_query = """
+                    mutation($item_id: ID!, $body: String!) {
+                    create_update(item_id: $item_id, body: $body) {
+                            id
+                          }
+                        }
+                        """
+                update_body = f"""Item created with the following details:
+                    Request From: {request_from}
+                    Contact Email: {contact_email}
+
+                    Request Number: {request_number}
+
+                    Subject: {subject}
+                    Description: {description}
+
+                    Archivist Assigned: {assigned_archivist}
+                    Date Entered: {formatted_date_entered}
+                    Assigned Date: {formatted_assigned_date}
+                    Date Completed: {formatted_date_completed}
+                    Due Date: {formatted_due_date}
+
+                    Work Type: {work_type}
+                    Calendar Tracking: {clnd_tracking}
+                    Request Source: {request_source}
+                    Model: {model}
+                    Critical: {formatted_critical}
+                    Request Status: {request_status}
+                    Tracking Info: {tracking_info}
+                    Material Link: {material_link}
+                    Presentation Date: {presentation_date_1}
+                    Presentation Audience: {presentation_audience}"""
+
+
+                create_update_variables = {
+                    "item_id": item_id,
+                    "body": update_body
+                }
+                create_update_data = {'query': create_update_query, 'variables': create_update_variables}
+
+                try:
+                    update_response = requests.post(url=apiUrl, json=create_update_data, headers=headers)
+                    update_response_data = update_response.json()
+                    if 'errors' in update_response_data:
+                        print(f"Error creating update: {update_response_data['errors']}")
+                    else:
+                        print(f"Update created for item ID: {item_id}")
+                except requests.exceptions.RequestException as e:
+                            print(f"Request failed: {e}")
+
+                # #Update column values: Thus Section for some reason is not working in updating Monday.com
+                # update_column_values_query = """
+                # mutation($board_id: ID!, $item_id: ID!, $column_values: JSON!) {
+                #   change_multiple_column_values(board_id: $board_id, item_id: $item_id, column_values: $column_values) {
+                #     id
+                #   }
+                # }
+                # """
+                # column_values = {
+                #     "status": {"label": "Done"},
+                #     "date4": {"date": formatted_due_date},
+                #     "email": {"email": "itsmyemail@mailserver.com", "text": "itsmyemail@mailserver.com"}
+                # }
+                # update_column_values_variables = {
+                #     "board_id": "",  # Ensure the board ID is a string
+                #     "item_id": item_id,        # Ensure the item ID is correct
+                #     "column_values": json.dumps(column_values)
+                # }
+                # update_column_values_data = {'query': update_column_values_query, 'variables': update_column_values_variables}
+
+                # try:
+                #     update_response = requests.post(url=apiUrl, json=update_column_values_data, headers=headers)
+                #     update_response_data = update_response.json()
+                #     if 'errors' in update_response_data:
+                #         print(f"Error updating column values: {update_response_data['errors']}")
+                #     else:
+                #         print(f"Column values updated for item ID: {item_id}")
+                # except requests.exceptions.RequestException as e:
+                #     print(f"Request failed: {e}")
+
         except requests.exceptions.RequestException as e:
             print(f"Request failed: {e}")
-
 
 def main():
     # Load the workbook and list all worksheets
